5cc4964aed30c943baf7657349f22c89090bc64c
Krishna Kumar <kumarkrishna9801552@gmail.com>
2025-05-19 08:18:57 +0530

    Your commit message

--- server/model/Contact.js
+++ server/model/Contact.js
@@ -1,9 +1,14 @@
 const mongoose = require("mongoose");
 
-const contactSchema = new mongoose.Schema({
-  name: { type: String, required: true },
-  email: { type: String, required: true },
-  message: { type: String, required: true },
-});
+const contactSchema = new mongoose.Schema(
+  {
+    name: { type: String, required: true },
+    email: { type: String, required: true },
+    message: { type: String, required: true },
+  },
+  {
+    timestamps: true, // <-- Automatically adds createdAt and updatedAt
+  }
+);
 
 module.exports = mongoose.model("Contact", contactSchema);
